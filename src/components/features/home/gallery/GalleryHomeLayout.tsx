afceade453fd7a000fb4ccd576eb1aba1791bf9a
marinaeleshaa <marinaeleshah77@gmail.com>
2025-12-03 15:13:10 +0200

    fix: update some responsiveness in gallery section

--- src/components/features/home/gallery/GalleryHomeLayout.tsx
+++ src/components/features/home/gallery/GalleryHomeLayout.tsx
@@ -4,9 +4,9 @@ import GalleryHomeRight from './GalleryHomeRight'
 
 const GalleryHomeLayout = () => {
   return (
-    <div className="flex flex-col md:flex-row md:gap-x-30 gap-5 justify-center items-center ">
+    <div className="flex flex-col md:flex-row md:gap-x-20 lg:gap-x-30 gap-5 justify-center items-center ">
         <GalleryHomeRight className="w-full md:w-1/2" />
-        <GalleryHomeLeft className="w-full md:w-1/2" />
+        <GalleryHomeLeft className="w-[80%] mx-auto md:w-1/2" />
     </div>
   )
 }
