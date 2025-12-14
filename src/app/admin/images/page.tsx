c5cf082e36045e347e712f0a2c92def2a8713c59
marinaeleshaa <marinaeleshah77@gmail.com>
2025-12-14 21:41:50 +0200

    wip: upload images with cloudinary

--- src/app/admin/images/page.tsx
+++ src/app/admin/images/page.tsx
@@ -3,12 +3,30 @@ import DashboardHero from "@/components/shared/dashboard/DashboardHero";
 import MasonryDashboard from "@/components/shared/dashboard/MasonaryDashboard";
 import Pagination from "@/components/ui/Pagination";
 import { images } from "@/data/images";
+import { CldUploadButton, CldUploadWidget } from "next-cloudinary";
 
 const page = () => {
   return (
     <div>
       <div className="space-y-10">
         <DashboardHero />
+
+        {/* <CldUploadButton
+          uploadPreset="<Upload Preset>"
+          className="bg-primary! px-4 py-2! rounded-lg! text-primary-foreground cursor-pointer hover:bg-primary/90 transition-all duration-300 hover:scale-105!"
+        /> */}
+
+        <CldUploadWidget
+          // uploadPreset="ElBeshara"
+          signatureEndpoint="/api/cloudinary/signature"
+          // options={{
+          //   multiple:false
+          // }}
+          // onUpload={(result)=> console.log("uploaded result",result)}
+        >
+          {({ open }) => <button onClick={() => open()}>Upload Image</button>}
+        </CldUploadWidget>
+
         <div className="bg-secondary/50 rounded-lg p-4">
           <div className="">
             <MasonryDashboard
@@ -16,7 +34,7 @@ const page = () => {
               onSelectionChange={(ids) => console.log("Selected images:", ids)}
               onRemove={(id) => console.log("Remove img:", id)}
             />
-            <Pagination  />
+            <Pagination />
           </div>
         </div>
       </div>
