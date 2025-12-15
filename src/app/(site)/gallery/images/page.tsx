3c99c23ac31c6c6ab3526b6ea1d488bf557ff5bd
marinaeleshaa <marinaeleshah77@gmail.com>
2025-12-15 17:26:41 +0200

    refactor: get-images route

--- src/app/(site)/gallery/images/page.tsx
+++ src/app/(site)/gallery/images/page.tsx
@@ -2,7 +2,6 @@
 import ImagesLayout from "@/components/features/gallery/images/ImagesLayout";
 import { getImagesServerAction } from "./actions";
 
-// Next.js 15: searchParams is a Promise
 interface PageProps {
   searchParams: Promise<{ page?: string }>;
 }
@@ -12,10 +11,9 @@ export default async function ImagesPage({ searchParams }: PageProps) {
   const params = await searchParams;
   const page = params.page ? Number(params.page) : 1;
 
-  // استدعاء السرفر أكشن مباشرة
-  const { data: images, meta } = await getImagesServerAction({ 
-    page, 
-    limit: 20 
+  const { data: images, meta } = await getImagesServerAction({
+    page,
+    limit: 20,
   });
 
   return (
